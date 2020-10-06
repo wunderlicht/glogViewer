c3caa373aaac9dc35246f30ef45ee7bf23a5c50c
wunderlicht <wunderlich@actualchange.eu>
2020-10-06 10:25:59 +0200

    add top bottom jump

--- glogViewer.go
+++ glogViewer.go
@@ -8,33 +8,66 @@ import (
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 	"image/color"
-
-	//"go/scanner"
 	"os"
-	//"strings"
 )
+
+type glogger struct {
+	autoScroll      bool
+	window          fyne.Window
+	scrollContainer widget.ScrollContainer
+	container       *fyne.Container
+	canvas          fyne.CanvasObject
+}
+
+func (g *glogger) handleTypedKey(ke *fyne.KeyEvent) {
+	g.scrollContainer.Scrolled(&fyne.ScrollEvent{
+		PointEvent: fyne.PointEvent{},
+		DeltaX:     0,
+		DeltaY:     0,
+	})
+}
+
+func (g *glogger) handleTypedRune(r rune) {
+	switch r {
+	case 't':
+		g.scrollContainer.ScrollToTop()
+		g.autoScroll = false
+	case 'b':
+		g.scrollContainer.ScrollToBottom()
+		g.autoScroll = true
+	}
+}
+
+func (g *glogger) setup(app fyne.App) {
+	g.autoScroll = true
+	g.window = app.NewWindow("glogViewer")
+	g.container = fyne.NewContainerWithLayout(layout.NewVBoxLayout())
+	g.scrollContainer = *widget.NewScrollContainer(g.container)
+	g.window.SetContent(&g.scrollContainer)
+	g.window.Canvas().SetOnTypedKey(g.handleTypedKey)
+	g.window.Canvas().SetOnTypedRune(g.handleTypedRune)
+}
 
 func main() {
 	myApp := app.New()
+	glog := new(glogger)
 
-	w := myApp.NewWindow("glogViewer")
-	container := fyne.NewContainerWithLayout(layout.NewVBoxLayout())
-	scrollContainer := widget.NewScrollContainer(container)
-	w.SetContent(scrollContainer)
+	glog.setup(myApp)
 
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			line := scanner.Text()
-			//appendLine(tg, line)
-			container.AddObject(canvas.NewText(line, color.White))
-			container.Refresh()
-			scrollContainer.ScrollToBottom()
+			glog.container.AddObject(canvas.NewText(line, color.White))
+			glog.container.Refresh()
+			if glog.autoScroll {
+				glog.scrollContainer.ScrollToBottom()
+			}
 		}
 		if err := scanner.Err(); err != nil {
 			panic(err)
 		}
 	}()
 
-	w.ShowAndRun()
+	glog.window.ShowAndRun()
 }
